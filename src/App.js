f83a869204789392f13fc6d847c08e2b70b55aed
Jeaninne <jeaninnel@hotmail.com>
2020-06-28 23:41:02 -0400

    installed eslint per Travis

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
+/* eslint-disable no-tabs */
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const num = 1;
+
+function number() {
+	return num;
+}
+
+number();
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+    Edit
+					<code>src/App.js</code>
+    and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+  Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
