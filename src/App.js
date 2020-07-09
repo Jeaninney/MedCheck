169520740415ebf403d67a19b90db2239e066a70
Jasmine Epps <epps.jasmine@gmail.com>
2020-07-08 20:49:18 -0400

    created layout of registration page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from './components/Header';
 
+import RegistrationForm from './components/RegistrationForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <RegistrationForm />
     </div>
   );
 }
