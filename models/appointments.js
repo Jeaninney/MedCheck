8b17d33b4160ad883cc3404e496daf6eea8eb39e
Jeaninne <jeaninnel@hotmail.com>
2020-07-10 20:25:14 -0400

    changed models for doctors and appointments

--- models/appointments.js
+++ models/appointments.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const appointmentsSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now },
+  purpose: { type: String, required: true },
+  apptStart: { type: Date, required: true },
+  apptEnd: { type: Date },
+  doctorId: { type: Number },
 });
 
 const Appointment = mongoose.model('Appointment', appointmentsSchema);
--- models/doctors.js
+++ models/doctors.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const doctorsSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now },
+  APIid: { type: String, required: true },
 });
 
 const Doctor = mongoose.model('Doctor', doctorsSchema);
