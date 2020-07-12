49d92453989a70a6f86f28d5c4795e1b9390ed82
Angela Stevenson <amsgwbootcamp@gmail.com>
2020-07-11 21:54:14 -0400

    medication api route working

---
+++ client/src/utils/API.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export default {
+    
+  // Gets all books
+  getMeds: function() {
+    return axios.get("/api/medications");
+  },
+  // Gets the book with the given id
+  getMedsId: function(id) {
+    return axios.get("/api/medications/" + id);
+  },
+  // Deletes the book with the given id
+  deleteMeds: function(id) {
+    return axios.delete("/api/medications/" + id);
+  },
+  // Saves a book to the database
+  saveMeds: function(medData) {
+    return axios.post("/api/medications", medData);
+  }
+};
