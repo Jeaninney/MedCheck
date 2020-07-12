aabf265e22b00e68eaaf54bec8ceef75f63873bd
Wilson <wilsonhorrell@git.training>
2020-07-12 10:22:19 -0500

    Added api routes

--- server.js
+++ server.js
@@ -14,23 +14,24 @@ const app = express();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+const apiRoutes = require("./routes/api");
+app.use(apiRoutes);
 const routes = require('./routes');
 
 app.use(routes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+    app.use(express.static('client/build'));
 }
 
 // Define API routes here
 // Define any API routes before this runs
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+    // eslint-disable-next-line no-console
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
