8780f5ebb8ad247fffa9efda7014402f01af6030
Ladislav Jerabek <ljerabek@microstrategy.com>
2020-07-14 00:00:57 -0400

    Fixing mistype in authentication logic in file models/users.js. Rearranging routes. Cleaning code.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
+const authRoutes = require('./auth');
 const appointmentRoutes = require('./appointments');
 const doctorRoutes = require('./doctors');
 const medicationRoutes = require('./medications');
 
 // Book routes
+router.use('/auth', authRoutes);
 router.use('/appointments', appointmentRoutes);
 router.use('/doctors', doctorRoutes);
 router.use('/medications', medicationRoutes);
