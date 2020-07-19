f414f7d2ed0cfd84e6d50b5ce192bd647a4c9846
Jeaninney <60117586+Jeaninney@users.noreply.github.com>
2020-07-19 11:09:33 -0400

    Update index.js

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function Header({ children }) {
+function Header() {
   return (
     <nav className="navbar navbar-dark bg-primary">
       <div className="row col-12 d-flex justify-content-center text-white">
-        <span className="h3">{children}</span>
+        <span className="h3">Register</span>
       </div>
     </nav>
   );
